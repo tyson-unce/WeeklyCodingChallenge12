0101e2cd5dd51ce4cc88502f15d1394467a56b6c
Tyson Unce <Tyson@MacBook-Pro.attlocal.net>
2022-06-19 05:59:42 -0500

    change database name

--- veryEasy.sql
+++ veryEasy.sql
@@ -1,27 +1,27 @@
-CREATE DATABASE very_easy;
-USE very_easy;
-CREATE TABLE top_three_cars(
+CREATE DATABASE weekly_GI;
+USE weekly_GI;
+CREATE TABLE very_easy(
 	make VARCHAR(20),
     model VARCHAR(20),
     year YEAR
     );
     
 SHOW TABLES;
-SELECT * FROM top_three_cars;
+SELECT * FROM very_easy;
 
-INSERT INTO top_three_cars (make,model,year)
+INSERT INTO very_easy (make,model,year)
 VALUES ('Toyota','Corolla',2021);
 
-INSERT INTO top_three_cars (make,model,year)
+INSERT INTO very_easy (make,model,year)
 VALUES ('Nissan','Rouge',2021);
 
-INSERT INTO top_three_cars (make,model,year)
+INSERT INTO very_easy (make,model,year)
 VALUES ('Jeep','Wrangler',2021);
 
-INSERT INTO top_three_cars (make,model,year)
+INSERT INTO very_easy (make,model,year)
 VALUES ('Ford','Explorer',2021);
 
-INSERT INTO top_three_cars (make,model,year)
+INSERT INTO very_easy (make,model,year)
 VALUES ('Subaru','Forester',2021);
 
 
